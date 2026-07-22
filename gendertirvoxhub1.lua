8d13cfb7be633fa9d76bc393774fbebac09049a0
RSL <tirvoxik@gmail.com>
2026-07-22 16:25:01 +0500

    Update gendertirvoxhub1.lua

--- gendertirvoxhub1.lua
+++ gendertirvoxhub1.lua
@@ -1,7 +1,7 @@
 -- [ ЗАГРУЗЧИК САТОШИ v1.3 - ОДИН СКРИПТ ] --
 
 local secret_key = "zhjkbn"
-local encrypted_linoria = ""
+local encrypted_linoria = "35.6.8.e.b.z.l.y.c.f.g.2a.16.n.m.u.2x.1k.x.1c.1b.1u.u.1e.2z.2x.t.1n.1f.1o.14.36.2u.2u.1f.1f.1e.30.1g.16.1r.1c.1m.1i.24.1y.1i.24.22.1l.1j.2b.1o.1i.1x.3i.1u.1q.22.3h.23.1r.2f.2l.26.2g.48.2b.2c.23.25.2b.29.2u.26.2t.2p.27.29.28.34.2j.4g.2f.34.2f.2j.4c.2m.2t.33.2w.30.3b.39.2v.2x.3k.2w.3b.3a.37.33.2x.3n.3t.53.32.3g.3b.58.4y.58.54.5i.5y"
 
 local function decrypt(data, key)
     local decrypted = ""
